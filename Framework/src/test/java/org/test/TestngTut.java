db6414975ecef8a46669320f4da2e962e4824bcf
Mtnmathan <mathanmtn27@gmail.com>
2022-12-24 13:08:20 +0530

    Just added one print statement

--- Framework/src/test/java/org/test/TestngTut.java
+++ Framework/src/test/java/org/test/TestngTut.java
@@ -5,16 +5,19 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class TestngTut {
 	public static void goooglesearch() {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "driver\\chromedriver_win32 (1)\\chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://google.com");
 		driver.findElement(By.name("q")).sendKeys("Mails");
 		driver.findElement(By.name("q")).sendKeys(Keys.RETURN);
 		String title = driver.getTitle();
 		System.out.println("The title of the page is "+title);
+		System.out.println("Pushed from file loc");
 		driver.close();
 		System.out.println("Git push");
 	}
